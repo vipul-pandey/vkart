896b2121f826b8a14f264aa4b971e468e3d007f1
Vipul Pandey <vipul1998pandey@gmail.com>
2025-09-22 00:13:19 +0530

    update server and frontend proxy port from 4000 to 3000

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
