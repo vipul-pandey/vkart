6049071ffb475e4c6433a3544730dc818965a08a
Vipul Pandey <vipul1998pandey@gmail.com>
2026-09-20 08:31:11 +0530

    fix: update axios instance baseURL for production environment

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5500"
-    : "/api";
+    : "";
 
 const instance = axios.create({
   baseURL,
