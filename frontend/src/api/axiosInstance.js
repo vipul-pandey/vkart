c172bec1ff7a846d1be20546d5114641519bcce3
Vipul Pandey <vipul1998pandey@gmail.com>
2026-09-20 07:27:09 +0530

    fix: update axios instance baseURL for API requests

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:5500"
-  baseURL: 'https://vkart-ry24.onrender.com'
+  baseURL: "http://13.204.82.250"
+  // baseURL: 'https://vkart-ry24.onrender.com'
 });
 
 export default instance;
